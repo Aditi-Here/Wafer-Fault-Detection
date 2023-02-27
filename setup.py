07ae06cb41df0407fadbc6c8883e7fbea291577c
Aditi-Here <hereaditi1@gmail.com>
2023-02-27 16:10:56 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    description="ML project",
+    author="Aditi Here",
+    packages=find_packages(),
+    license="MIT"
+)
